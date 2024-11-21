107ed0603d2fa57d75510810c9a7003254734a11
Nemozikr <ebhomrah@gmail.com>
2024-11-21 14:23:09 +0000

    Added grammar check function

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const words = ['apple', 'ATEEZ', 'banana', 'boom', 'cucumber', 'commander', 'chk-chk', 'delta', 'demure', 'delicious', 'ecstatic', 'ecosystem', 'eco-round', 'fr', 'fun', 'F', 'function', 'facts', 'gay', 'greasy', 'Gary', 'girlie', 'homo', 'Himalayas', 'hermit', 'haram', 'halal', 'hello', 'hi', 'hippopotomonstrosesquippedaliophobia', 'hippo', 'huge', 'hehe', 'hehehehaw', 'her', 'his', 'him', 'hersheys', 'HALA', 'invoice', 'invitation', 'invoke', 'illegal', 'irregular', 'ingles', 'Instagram', 'jajajaja', 'Joker', 'jizz', 'jazz', 'Jalil', 'joemama', 'jungle', 'kalm', 'KKK', 'key', 'kitten', 'kebab', 'kick', 'kindergarteners', 'kaleidoscopical', 'Li', 'L', 'loser', 'ligma', 'lacking', 'labour', 'labouriousnessess', 'lobotomy', 'lobotomisations', 'my', 'me', 'mad', 'man', 'macaron', 'madman', 'maggot', 'macrolinguistic',  'nidda', 'no', 'Nero', 'nerd', 'nap', 'naan', 'nachos', 'narrow', 'nasty', 'names', 'name', 'naive', 'narcoterrorism', 'oh', 'ofc', 'of', 'oak', 'oath', 'oat', 'oasis', 'oblong', 'object', 'obliviousness', 'omg', 'pp', 'pfp', 'penis', 'play', 'pluck', 'paki', 'PEW PEW PEW', 'packing', 'packs', 't', 'turrents', 'turrets' , 'p', 'pneumonoultramicroscopicsilicovolcanoconiosis'];
-const starts = ['a', 'an', 'is', 'the', 'of', 'it', 'what'];
+const words = ['apple', 'ATEEZ', 'banana', 'boom', 'cucumber', 'commander', 'chk-chk', 'delta', 'demure', 'delicious', 'ecstatic', 'ecosystem', 'eco-round', 'fr', 'fun', 'F', 'function', 'facts', 'gay', 'greasy', 'Gary', 'girlie', 'homo', 'Himalayas', 'hermit', 'haram', 'halal', 'hello', 'hi', 'hippopotomonstrosesquippedaliophobia', 'hippo', 'huge', 'hehe', 'hehehehaw', 'her', 'his', 'him', 'hersheys', 'HALA', 'invoice', 'invitation', 'invoke', 'illegal', 'irregular', 'ingles', 'Instagram', 'jajajaja', 'Joker', 'jizz', 'jazz', 'Jalil', 'joemama', 'jungle', 'kalm', 'KKK', 'key', 'kitten', 'kebab', 'kick', 'kindergarteners', 'kaleidoscopical', 'Li', 'L', 'loser', 'ligma', 'lacking', 'labour', 'labouriousnessess', 'lobotomy', 'lobotomisations', 'my', 'me', 'mad', 'man', 'macaron', 'madman', 'maggot', 'macrolinguistic', 'NCT', 'nidda', 'no', 'Nero', 'nerd', 'nap', 'naan', 'nachos', 'narrow', 'nasty', 'names', 'name', 'naive', 'narcoterrorism', 'oh', 'ofc', 'of', 'oak', 'oath', 'oat', 'oasis', 'oblong', 'object', 'obliviousness', 'omg', 'pp', 'pfp', 'penis', 'play', 'pluck', 'paki', 'PEW PEW PEW', 'packing', 'packs', 't', 'turrents', 'turrets', 's', 'SKZ' , 'p', 'pneumonoultramicroscopicsilicovolcanoconiosis'];
+const starts = ['a', 'an', 'and', 'is', 'the', 'of', 'it', 'what'];
 const punct = [".", ",", "!", "?", "!?"];
+const vowels = ['a', 'e', 'i', 'o', 'u'];
 let message = [];
+sentence = '';
 
 function randomSize(max = 10, min = 1) {
     return  Math.floor(Math.random() * max) + min;
@@ -24,8 +26,8 @@ function randomWord() {
 }
 
 function makePhrase(num = randomSize()) {
-    let sentence = (`${randomConnective()} ${makeMessage(num)}${randomPunct()}`);
-    console.log(sentence);
+    sentence = (`${randomConnective()} ${makeMessage(num)}${randomPunct()}`);
+    //console.log(sentence);
     sentence = sentence.charAt(0).toUpperCase() + sentence.slice(1);
     return sentence;
 }
@@ -40,4 +42,25 @@ function makeMessage(count) {
     return message.join(" ");
 }
 
+function grammarCheck() {
+    let temp = sentence.split(' ');
+    //console.log(temp[1]) // second word
+    if (temp[0] === 'A' || temp[0] === 'An'){
+        vowels.forEach(vowel => {
+            //console.log(temp);
+            let firstLetterOfSecondWord = temp[1].charAt(0);
+            if (firstLetterOfSecondWord.toLowerCase() === vowel) {
+                temp[0] = 'An';
+            } else if (firstLetterOfSecondWord.toLowerCase() !== vowel) {
+                temp[0] = 'A';
+            } else {
+                throw Error("Error at grammar check on vowels, what did you do?");
+            }
+        })
+    }
+    //console.log(temp)
+    return temp.join(" ");
+}
+
 console.log(makePhrase());
+console.log(grammarCheck())
